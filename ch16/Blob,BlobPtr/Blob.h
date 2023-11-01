91d1ecbc1aff12d7679b26e12a6718f5d179847c
pi big star <103993642+qcy1024@users.noreply.github.com>
2023-11-01 11:54:58 +0800

    Add files via upload

--- ch16/Blob,BlobPtr/Blob.h
+++ ch16/Blob,BlobPtr/Blob.h
@@ -3,8 +3,10 @@
 #include <memory>
 #include <initializer_list>
 #include <stdexcept>
+#include "BlobPtr.h"
 template<typename T> class Blob
 {
+	friend class BlobPtr<T>;
 public:
 	typedef typename std::vector<T>::size_type size_type;
 
@@ -12,10 +14,15 @@ public:
 	Blob<T>() : data(std::make_shared<std::vector<T> >()) {};
 	Blob<T>(std::initializer_list<T> li) : data(std::make_shared<std::vector<T> >(li)) {};
 	
-	//接口
+	/*接口*/
 	void push_back(const T& elem);
+	void pop_back(const T& elem);
 	T& operator [] (size_type i);
-	size_type size() { return data->size(); };
+	size_type size() const { return data->size(); };
+	bool empty() const { return data->empty(); }
+	//返回首尾迭代器
+	BlobPtr<T> begin() const;
+	BlobPtr<T> end() const;
 
 private:
 	std::shared_ptr<std::vector<T> > data;
@@ -37,9 +44,29 @@ template<typename T> void Blob<T>::push_back(const T& elem)
 	return;
 }
 
+//pop_back
+template <typename T> void Blob<T>::pop_back(const T& elem)
+{
+	data->pop_back();
+	return;
+}
+
 //下标访问运算符
-template<typename T> T& Blob<T>::operator [] (size_type i)
+template<typename T> T& Blob<T>::operator [] (size_type i) 
 {
 	check(i, "out of range");
 	return (*data)[i];
+}
+
+//begin()和end()返回首尾迭代器
+template <typename T> BlobPtr<T> Blob<T>::begin() const
+{
+	BlobPtr<T> ret(*this, 0);
+	return ret;
+}
+
+template <typename T> BlobPtr<T> Blob<T>::end() const
+{
+	BlobPtr<T> ret(*this, 0 + this->size());
+	return ret;
 }
--- ch16/Blob,BlobPtr/BlobPtr.h
+++ ch16/Blob,BlobPtr/BlobPtr.h
@@ -1,6 +1,48 @@
 #pragma once
+#include <memory>
+#include <vector>
+#include <string>
+#include <stdexcept>
+template <typename T> class Blob;
 template<typename T> class BlobPtr
 { 
+	typedef typename std::vector<T>::size_type size_type;
+public:
+	/*构造函数与拷贝控制*/
+	//创建迭代器但并不让它指向任何地方，weak_ptr可以不被初始化，此时迭代器的指向是未定义的
+	BlobPtr<T>() : curr(0) { };
+	BlobPtr<T>(const Blob<T>& tmpBlob, size_type idx) : curr(idx), wptr(tmpBlob.data) {};
 
+	/*接口*/
+	//解引用运算符
+	T& operator * ();
+
+
+private:
+	//weak_ptr，弱共享数据
+	std::weak_ptr<std::vector<T> > wptr;
+	//工具函数，检查weak_ptr指向的数据是否存在，以及下标位置是否合法
+	std::shared_ptr<std::vector<T> > check(size_type idx, const std::string msg);
+	//当前位置
+	size_type curr;
 };
 
+//工具函数，检查weak_ptr指向的数据是否存在，以及下标位置是否合法，若检查通过返回指向数据的shared_ptr
+//试图用迭代器访问已经不存在的数据，或者不合法的下标位置将引发异常。
+template <typename T>
+std::shared_ptr<std::vector<T> > BlobPtr<T>::check(size_type idx, const std::string msg)
+{
+	std::shared_ptr<std::vector<T> > p = wptr.lock();
+	if (!p)
+		throw std::runtime_error("unbound Blob");
+	if (idx >= p->size())
+		throw std::out_of_range(msg);
+	return p;
+}
+
+//解引用运算符
+template <typename T> T& BlobPtr<T>::operator * ()
+{
+	std::shared_ptr<std::vector<T> > p = check(curr, "out of range");
+	return (*p)[curr];
+}
